45d34f4d437c95f63888bc7c9efb77a519a31e67
fast40 <eclfast@gmail.com>
2026-01-15 12:44:40 -0800

    graph improvements

--- gpt2_layer_norms.py
+++ gpt2_layer_norms.py
@@ -1,3 +1,5 @@
+import functools
+
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import matplotlib.pyplot as plt
@@ -9,13 +11,14 @@ tokenizer = AutoTokenizer.from_pretrained('gpt2')
 inputs = tokenizer('Here is some test text.', return_tensors='pt')
 
 
-def get_norms(model, inputs):
+def inspect_hidden_states(model, inputs, inspector_func):
     norms = []
     hooks = []
 
     for gpt_block in model.transformer.h:
-        hooks.append(gpt_block.register_forward_hook(
-            lambda module, inputs, outputs: norms.append(outputs[0].norm(dim=-1).mean()))
+        print(gpt_block.named_modules)
+        hooks.append(gpt_block.mlp.register_forward_hook(
+            lambda module, inputs, outputs: norms.append(inspector_func(outputs[0], dim=-1).mean()))
         )
 
     with torch.no_grad():
@@ -36,10 +39,22 @@ ax.xaxis.set_major_locator(mticker.MultipleLocator(1))
 plt.grid(which='major')
 plt.grid(which='minor', alpha=0.3)
 
-for i in range(4, 25 + 1):
-    norms = get_norms(model, inputs)
-    plt.plot(norms, marker='o')
+# for i in range(4, 25 + 1):
 
+x_values = [f'layer #{i}' for i in range(1, len(model.transformer.h) + 1)]
 
+norms = inspect_hidden_states(model, inputs, torch.norm)
+plt.plot(x_values, norms, marker='o', label='norms')
+
+means = inspect_hidden_states(model, inputs, torch.mean)
+plt.plot(x_values, means, marker='o', label='means')
+
+means = inspect_hidden_states(model, inputs, torch.var)
+plt.plot(x_values, means, marker='o', label='var')
+
+plt.title('Average Mean, L2 Norm, and Variance of Residual Contributions')
+plt.xlabel('Layer Number')
+plt.ylabel('Mean, Variance, or L2 Norm')
+plt.legend()
 plt.ylim(0)
 plt.show()
